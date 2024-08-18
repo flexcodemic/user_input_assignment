f80ba4088f8a2e2c32d9acae8cbc620574dd047a
Anthony Aruma <anthonyaruma091@gmail.com>
2024-08-18 21:50:45 +0100

    Ask for user input

---
+++ user_input.py
@@ -0,0 +1,12 @@
+# Ask user for their name
+name = input('What is your name? ')
+
+# Ask user for their age
+age = input('How old are you? ')
+
+# Ask user for their location
+location = input('Where do you live? ')
+
+# Output 
+print(f'Welcome on board {name}! You are {age} years old and lives in {location}')
+
